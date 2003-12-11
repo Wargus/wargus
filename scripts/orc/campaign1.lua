956a6311d2ac7f8be63132b28f02e9139b72ce99
jsalmon3 <>
2003-12-11 21:52:01 +0000

    Initial lua support

---
+++ scripts/orc/campaign1.lua
@@ -0,0 +1,134 @@
+--       _________ __                 __                               
+--      /   _____//  |_____________ _/  |______     ____  __ __  ______
+--      \_____  \\   __\_  __ \__  \\   __\__  \   / ___\|  |  \/  ___/
+--      /        \|  |  |  | \// __ \|  |  / __ \_/ /_/  >  |  /\___ \ 
+--     /_______  /|__|  |__|  (____  /__| (____  /\___  /|____//____  >
+--             \/                  \/          \//_____/            \/ 
+--  ______________________                           ______________________
+--			  T H E   W A R   B E G I N S
+--	   Stratagus - A free fantasy real time strategy game engine
+--
+--	campaign1.ccl	-	Define the orc campaign 1.
+--
+--	(c) Copyright 2002 by Lutz Sammer
+--
+--      This program is free software; you can redistribute it and/or modify
+--      it under the terms of the GNU General Public License as published by
+--      the Free Software Foundation; either version 2 of the License, or
+--      (at your option) any later version.
+--  
+--      This program is distributed in the hope that it will be useful,
+--      but WITHOUT ANY WARRANTY; without even the implied warranty of
+--      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--      GNU General Public License for more details.
+--  
+--      You should have received a copy of the GNU General Public License
+--      along with this program; if not, write to the Free Software
+--      Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+--
+--	$Id$
+
+--=============================================================================
+--	Define the campaign
+--
+--	(define-campagin 'ident 'name "name" 'campaign (list elements)
+DefineCampaign("orc", "name", "~!Orc campaign",
+  "campaign", {
+	"show-picture", {
+	  "image", "campaigns/orc/interface/Act_I_-_Seas_of_Blood.png",
+	  "fade-in", 30 * 2,
+	  "fade-out", 30 * 2,
+	  "display-time", 30 * 10,
+	  "text", {
+	    "font", "large-title",
+	    "x", 640 / 2,
+	    "y", (480 / 2) - 67,
+	    "width", 640,
+	    "height", 0,
+	    "align", "center",
+	    "text", "Act I"},
+	  "text", {
+	    "font", "small-title",
+	    "x", 640 / 2,
+	    "y", (480 / 2) - 25,
+	    "width", 640,
+	    "height", 0,
+	    "align", "center",
+	    "text", "Seas of Blood"}},
+	"play-level", "campaigns/orc/level01o.cm",
+	"play-level", "campaigns/orc/level02o.cm",
+	"play-level", "campaigns/orc/level03o.cm",
+	"play-level", "campaigns/orc/level04o.cm",
+	"show-picture", {
+	  "image", "campaigns/orc/interface/Act_II_-_Khaz_Modan.png",
+	  "fade-in", 30 * 2,
+	  "fade-out", 30 * 2,
+	  "display-time", 30 * 10,
+	  "text", {
+	    "font", "large-title",
+	    "x", 640 / 2,
+	    "y", (480 / 2) - 67,
+	    "width", 640,
+	    "height", 0,
+	    "align", "center",
+	    "text", "Act II"},
+	  "text", {
+	    "font", "small-title",
+	    "x", 640 / 2,
+	    "y", (480 / 2) - 25,
+	    "width", 640,
+	    "height", 0,
+	    "align", "center",
+	    "text", "Khaz Modan"}},
+	"play-level", "campaigns/orc/level05o.cm",
+	"play-level", "campaigns/orc/level06o.cm",
+	"play-level", "campaigns/orc/level07o.cm",
+	"show-picture", {
+	  "image", "campaigns/orc/interface/Act_III_-_Quel'Thalas.png",
+	  "fade-in", 30 * 2,
+	  "fade-out", 30 * 2,
+	  "display-time", 30 * 10,
+	  "text", {
+	    "font", "large-title",
+	    "x", 640 / 2,
+	    "y", (480 / 2) - 67,
+	    "width", 640,
+	    "height", 0,
+	    "align", "center",
+	    "text", "Act III"},
+	  "text", {
+	    "font", "small-title",
+	    "x", 640 / 2,
+	    "y", (480 / 2) - 25,
+	    "width", 640,
+	    "height", 0,
+	    "align", "center",
+	    "text", "Quel'Thalas"}},
+	"play-level", "campaigns/orc/level08o.cm",
+	"play-level", "campaigns/orc/level09o.cm",
+	"play-level", "campaigns/orc/level10o.cm",
+	"play-level", "campaigns/orc/level11o.cm",
+	"show-picture", {
+	  "image", "campaigns/orc/interface/Act_IV_-_Tides_of_Darkness.png",
+	  "fade-in", 30 * 2,
+	  "fade-out", 30 * 2,
+	  "display-time", 30 * 10,
+	  "text", {
+	    "font", "large-title",
+	    "x", 640 / 2,
+	    "y", (480 / 2) - 67,
+	    "width", 640,
+	    "height", 0,
+	    "align", "center",
+	    "text", "ACT IV"},
+	  "text", {
+	    "font", "small-title",
+	    "x", 640 / 2,
+	    "y", (480 / 2) - 25,
+	    "width", 640,
+	    "height", 0,
+	    "align", "center",
+	    "text", "Tides of Darkness"}},
+	"play-level", "campaigns/orc/level12o.cm",
+	"play-level", "campaigns/orc/level13o.cm",
+	"play-level", "campaigns/orc/level14o.cm" } )
---
+++ scripts/orc/campaign2.lua
@@ -0,0 +1,128 @@
+--       _________ __                 __                               
+--      /   _____//  |_____________ _/  |______     ____  __ __  ______
+--      \_____  \\   __\_  __ \__  \\   __\__  \   / ___\|  |  \/  ___/
+--      /        \|  |  |  | \// __ \|  |  / __ \_/ /_/  >  |  /\___ \ 
+--     /_______  /|__|  |__|  (____  /__| (____  /\___  /|____//____  >
+--             \/                  \/          \//_____/            \/ 
+--  ______________________                           ______________________
+--			  T H E   W A R   B E G I N S
+--	   Stratagus - A free fantasy real time strategy game engine
+--
+--	campaign2.ccl	-	Define the orc campaign 2.
+--
+--	(c) Copyright 2002 by Lutz Sammer
+--
+--      This program is free software; you can redistribute it and/or modify
+--      it under the terms of the GNU General Public License as published by
+--      the Free Software Foundation; either version 2 of the License, or
+--      (at your option) any later version.
+--  
+--      This program is distributed in the hope that it will be useful,
+--      but WITHOUT ANY WARRANTY; without even the implied warranty of
+--      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--      GNU General Public License for more details.
+--  
+--      You should have received a copy of the GNU General Public License
+--      along with this program; if not, write to the Free Software
+--      Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+--
+--	$Id$
+
+DefineCampaign("orc-exp", "name", "O~!rc expansion levels",
+  "campaign", {
+	"show-picture", {
+	  "image", "campaigns/orc-exp/interface/Act_I_-_Draenor,_the_Red_World.png",
+	  "fade-in", 30 * 2,
+	  "fade-out", 30 * 2,
+	  "display-time", 30 * 10,
+	  "text", {
+	    "font", "large-title",
+	    "x", 640 / 2,
+	    "y", (480 / 2) - 67,
+	    "width", 640,
+	    "height", 0,
+	    "align", "center",
+	    "text", "Act I"},
+	  "text", {
+	    "font", "small-title",
+	    "x", 640 / 2,
+	    "y", (480 / 2) - 25,
+	    "width", 640,
+	    "height", 0,
+	    "align", "center",
+	    "text", "Draenor, the Red World"}},
+	"play-level", "campaigns/orc-exp/levelx01o.cm",
+	"play-level", "campaigns/orc-exp/levelx02o.cm",
+	"play-level", "campaigns/orc-exp/levelx03o.cm",
+	"show-picture", {
+	  "image", "campaigns/orc-exp/interface/Act_II_-_The_Burning_of_Azeroth.png",
+	  "fade-in", 30 * 2,
+	  "fade-out", 30 * 2,
+	  "display-time", 30 * 10,
+	  "text", {
+	    "font", "large-title",
+	    "x", 640 / 2,
+	    "y", (480 / 2) - 67,
+	    "width", 640,
+	    "height", 0,
+	    "align", "center",
+	    "text", "Act II"},
+	  "text", {
+	    "font", "small-title",
+	    "x", 640 / 2,
+	    "y", (480 / 2) - 25,
+	    "width", 640,
+	    "height", 0,
+	    "align", "center",
+	    "text", "The Burning of Azeroth"}},
+	"play-level", "campaigns/orc-exp/levelx04o.cm",
+	"play-level", "campaigns/orc-exp/levelx05o.cm",
+	"play-level", "campaigns/orc-exp/levelx06o.cm",
+	"show-picture", {
+	  "image", "campaigns/orc-exp/interface/Act_III_-_The_Great_Sea.png",
+	  "fade-in", 30 * 2,
+	  "fade-out", 30 * 2,
+	  "display-time", 30 * 10,
+	  "text", {
+	    "font", "large-title",
+	    "x", 640 / 2,
+	    "y", (480 / 2) - 67,
+	    "width", 640,
+	    "height", 0,
+	    "align", "center",
+	    "text", "Act III"},
+	  "text", {
+	    "font", "small-title",
+	    "x", 640 / 2,
+	    "y", (480 / 2) - 25,
+	    "width", 640,
+	    "height", 0,
+	    "align", "center",
+	    "text", "The Great Sea"}},
+	"play-level", "campaigns/orc-exp/levelx07o.cm",
+	"play-level", "campaigns/orc-exp/levelx08o.cm",
+	"play-level", "campaigns/orc-exp/levelx09o.cm",
+	"show-picture", {
+	  "image", "campaigns/orc-exp/interface/Act_IV_-_Prelude_to_New_Worlds.png",
+	  "fade-in", 30 * 2,
+	  "fade-out", 30 * 2,
+	  "display-time", 30 * 10,
+	  "text", {
+	    "font", "large-title",
+	    "x", 640 / 2,
+	    "y", (480 / 2) - 67,
+	    "width", 640,
+	    "height", 0,
+	    "align", "center",
+	    "text", "Act IV"},
+	  "text", {
+	    "font", "small-title",
+	    "x", 640 / 2,
+	    "y", (480 / 2) - 25,
+	    "width", 640,
+	    "height", 0,
+	    "align", "center",
+	    "text", "Prelude to New Worlds"}},
+	"play-level", "campaigns/orc-exp/levelx10o.cm",
+	"play-level", "campaigns/orc-exp/levelx11o.cm",
+	"play-level", "campaigns/orc-exp/levelx12o.cm" } )
