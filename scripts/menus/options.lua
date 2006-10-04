813fe9b4f3679693d102c6ba1aa855c668f03e23
jsalmon3 <>
2006-10-04 05:06:05 +0000

    Added game sound options

--- scripts/menus/options.lua
+++ scripts/menus/options.lua
@@ -1,9 +1,100 @@
+function AddSoundOptions(menu, offx, offy, centerx, bottom)
+  local b
+
+  b = menu:addLabel("Sound Options", 176, 11)
+
+  b = Label("Effects Volume")
+  b:setFont(CFont:Get("game"))
+  b:adjustSize();
+  menu:add(b, offx + 16, offy + 36 * 1)
+
+  -- FIXME: disable if effects turned off
+  local soundslider = Slider(0, 255)
+  soundslider:setValue(GetEffectsVolume())
+  soundslider:setActionCallback(function() SetEffectsVolume(soundslider:getValue()) end)
+  soundslider:setWidth(198)
+  soundslider:setHeight(18)
+  soundslider:setBaseColor(dark)
+  soundslider:setForegroundColor(clear)
+  soundslider:setBackgroundColor(clear)
+  menu:add(soundslider, offx + 32, offy + 36 * 1.5)
+
+  b = Label("min")
+  b:setFont(CFont:Get("game"))
+  b:adjustSize();
+  menu:addCentered(b, offx + 44, offy + 36 * 2 + 6)
+
+  b = Label("max")
+  b:setFont(CFont:Get("game"))
+  b:adjustSize();
+  menu:addCentered(b, offx + 218, offy + 36 * 2 + 6)
+
+  local effectscheckbox = {}
+  effectscheckbox = menu:addCheckBox("Enabled", offx + 240, offy + 36 * 1.5,
+    function() SetEffectsEnabled(effectscheckbox:isMarked()) end)
+  effectscheckbox:setMarked(IsEffectsEnabled())
+  effectscheckbox:adjustSize()
+
+  b = Label("Music Volume")
+  b:setFont(CFont:Get("game"))
+  b:adjustSize();
+  menu:add(b, offx + 16, offy + 36 * 3)
+
+  -- FIXME: disable if music turned off
+  local musicslider = Slider(0, 255)
+  musicslider:setValue(GetMusicVolume())
+  musicslider:setActionCallback(function() SetMusicVolume(musicslider:getValue()) end)
+  musicslider:setWidth(198)
+  musicslider:setHeight(18)
+  musicslider:setBaseColor(dark)
+  musicslider:setForegroundColor(clear)
+  musicslider:setBackgroundColor(clear)
+  menu:add(musicslider, offx + 32, offy + 36 * 3.5)
+
+  b = Label("min")
+  b:setFont(CFont:Get("game"))
+  b:adjustSize();
+  menu:addCentered(b, offx + 44, offy + 36 * 4 + 6)
+
+  b = Label("max")
+  b:setFont(CFont:Get("game"))
+  b:adjustSize();
+  menu:addCentered(b, offx + 218, offy + 36 * 4 + 6)
+
+  local musiccheckbox = {}
+  musiccheckbox = menu:addCheckBox("Enabled", offx + 240, offy + 36 * 3.5,
+    function() SetMusicEnabled(musiccheckbox:isMarked()); MusicStopped() end)
+  musiccheckbox:setMarked(IsMusicEnabled())
+  musiccheckbox:adjustSize();
+
+  b = menu:addFullButton("~!OK", "o", centerx, bottom - 11 - 27,
+    function()
+      preferences.EffectsVolume = GetEffectsVolume()
+      preferences.EffectsEnabled = IsEffectsEnabled()
+      preferences.MusicVolume = GetMusicVolume()
+      preferences.MusicEnabled = IsMusicEnabled()
+      SavePreferences()
+      menu:stop()
+    end)
+end
+
+function RunGameSoundOptionsMenu(s)
+  local menu = WarGameMenu(hpanel5)
+  menu:setSize(352, 352)
+  menu:setPosition((Video.Width - menu:getWidth()) / 2,
+    (Video.Height - menu:getHeight()) / 2)
+
+  AddSoundOptions(menu, 0, 0, 352/2 - 224/2, 352)
+
+  menu:run(false)
+end
+
 function RunGameOptionsMenu()
   local menu = WarGameMenu(hpanel1)
 
   menu:addLabel("Game Options", 128, 11)
   menu:addFullButton("Sound (~<F7~>)", "f7", 16, 40 + 36*0,
-    function() end)
+    function() RunGameSoundOptionsMenu() end)
   menu:addFullButton("Speeds (~<F8~>)", "f8", 16, 40 + 36*1,
     function() end)
   menu:addFullButton("Preferences (~<F9~>)", "f9", 16, 40 + 36*2,
