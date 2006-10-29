8f1562c75a7496c42ff8685da804d2e58570dff5
jsalmon3 <>
2006-10-29 01:43:17 +0000

    Removed MenuPanel

--- scripts/human/ui.lua
+++ scripts/human/ui.lua
@@ -270,15 +270,3 @@ UI.NetworkDiplomacyButton.Text = "Diplomacy"
 UI.NetworkDiplomacyButton.Style = FindButtonStyle("network")
 UI.NetworkDiplomacyButton:SetCallback(function() RunDiplomacyMenu() end)
 
-function AddMenuPanel(ident, file)
-	b = CMenuPanel:new_local()
-	b.Ident = ident
-	b.G = CGraphic:New(file)
-	UI.MenuPanels:push_back(b)
-end
-
-AddMenuPanel("panel1", "ui/human/panel_1.png")
-AddMenuPanel("panel2", "ui/human/panel_2.png")
-AddMenuPanel("panel3", "ui/human/panel_3.png")
-AddMenuPanel("panel4", "ui/human/panel_4.png")
-AddMenuPanel("panel5", "ui/human/panel_5.png")
