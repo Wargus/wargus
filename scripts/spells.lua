f1c0a235ebd7b43f57e87b8a4b3fd62593a4e963
jsalmon3 <>
2007-01-07 00:56:53 +0000

    Added range 1 to suicide bomber

--- scripts/spells.lua
+++ scripts/spells.lua
@@ -82,6 +82,7 @@ DefineSpell("spell-suicide-bomber", {
 
 DefineSpell("spell-suicide-bomber",
 	"showname", "Demolish", "manacost", 0, "target", "self",
+    "range", 1,
 	"action", {{"demolish", "range", 1, "damage", 400},
 		{"spawn-missile", "missile", "missile-normal-spell",
 			"end-point",   {"base", "caster"}}},
