b4287d05c5edbace0bd0534457907fc879afbb69
Pali Rohár <pali.rohar@gmail.com>
2011-11-06 20:38:41 +0100

    Add cmake module for Stratagus, generate wargus binary with correct CMAKE_INSTALL_PREFIX path

---
+++ cmake/modules/FindStratagus.cmake
@@ -0,0 +1,28 @@
+# - Try to find the stratagus executable and game headers
+# Once done this will define
+#
+#  STRATAGUS_FOUND - system has stratagus
+#  STRATAGUS - the stratagus executable
+#  STRATAGUS_INCLUDE_DIR - the stratagus include directory
+
+# Copyright (c) 2011, Pali Rohár <pali.rohar@gmail.com>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+if(STRATAGUS AND STRATAGUS_INCLUDE_DIR)
+	set(STRATAGUS_FOUND true)
+else()
+	find_program(STRATAGUS NAMES stratagus)
+	find_path(STRATAGUS_INCLUDE_DIR stratagus-game-launcher.h)
+
+	if(STRATAGUS AND STRATAGUS_INCLUDE_DIR)
+		set(STRATAGUS_FOUND true)
+		message(STATUS "Found stratagus: ${STRATAGUS}:${STRATAGUS_INCLUDE_DIR}")
+	else()
+		set(STRATAGUS_FOUND false)
+		message(FATAL_ERROR "Could not find stratagus")
+	endif()
+
+	mark_as_advanced(STRATAGUS STRATAGUS_INCLUDE_DIR)
+endif()
