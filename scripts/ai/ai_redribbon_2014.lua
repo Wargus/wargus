e73984787ccc13c7d16e594a1fb433c138ed4c62
Kyran Jackson <erstmap@gmail.com>
2014-09-08 17:46:55 +1000

    [+] Added Mushroom Panic, a map which uses FtM2014.

--- scripts/ai/ai_redribbon_2014.lua
+++ scripts/ai/ai_redribbon_2014.lua
@@ -27,103 +27,173 @@
 --      Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 --
 
-local ftm_unit = {}
-local ftm_origin = {}
-local ftm_cost = {}
-local ftm_origin_x = {}
-local ftm_origin_y = {}
-
-local ftm_team = {}
-local ftm_team_x1 = {}
-local ftm_team_y1 = {}
-local ftm_team_x2 = {}
-local ftm_team_y2 = {}
-
-local ftm_choice = {} -- Who gets spawned in next.
-
 local timer = {}
 
 function AiRedRibbon_Setup_2014()
+	ftm_team = {}
+	ftm_team_x1 = {}
+	ftm_team_y1 = {}
+	ftm_team_x2 = {}
+    ftm_team_y2 = {}
+	ftm_unit = {}
+	ftm_origin = {}
+	ftm_cost = {}
+	ftm_origin_x = {}
+	ftm_origin_y = {}
+	ftm_choice = {} -- Who gets spawned in next.
+	ftm_index_start = {}
+	ftm_index_end = {}
 	ftm_unit[1] = "unit-footman"
 	ftm_origin[1] = "unit-human-barracks"
 	ftm_cost[1] = 25
-	ftm_origin_x[1] = 150
-	ftm_origin_y[1] = 150
+	ftm_origin_x[1] = 1
+	ftm_origin_y[1] = 1
 	ftm_unit[2] = "unit-arthor-literios"
 	ftm_origin[2] = "unit-human-barracks"
 	ftm_cost[2] = 200
-	ftm_origin_x[2] = 150
-	ftm_origin_y[2] = 150
+	ftm_origin_x[2] = 1
+	ftm_origin_y[2] = 1
 	ftm_unit[3] = "unit-archer"
 	ftm_origin[3] = "unit-human-barracks"
 	ftm_cost[3] = 25
-	ftm_origin_x[3] = 150
-	ftm_origin_y[3] = 150
+	ftm_origin_x[3] = 1
+	ftm_origin_y[3] = 1
 	ftm_unit[4] = "unit-ranger"
 	ftm_origin[4] = "unit-human-barracks"
 	ftm_cost[4] = 50
-	ftm_origin_x[4] = 150
-	ftm_origin_y[4] = 150
+	ftm_origin_x[4] = 1
+	ftm_origin_y[4] = 1
 	ftm_unit[5] = "unit-female-hero"
 	ftm_origin[5] = "unit-human-barracks"
 	ftm_cost[5] = 200
-	ftm_origin_x[5] = 150
-	ftm_origin_y[5] = 150
+	ftm_origin_x[5] = 1
+	ftm_origin_y[5] = 1
 	ftm_unit[6] = "unit-knight"
 	ftm_origin[6] = "unit-human-barracks"
 	ftm_cost[6] = 75
-	ftm_origin_x[6] = 150
-	ftm_origin_y[6] = 150
+	ftm_origin_x[6] = 1
+	ftm_origin_y[6] = 1
 	ftm_unit[7] = "unit-paladin"
 	ftm_origin[7] = "unit-human-barracks"
 	ftm_cost[7] = 125
-	ftm_origin_x[7] = 150
-	ftm_origin_y[7] = 150
+	ftm_origin_x[7] = 1
+	ftm_origin_y[7] = 1
 	ftm_unit[8] = "unit-knight-rider"
 	ftm_origin[8] = "unit-human-barracks"
 	ftm_cost[8] = 200
-	ftm_origin_x[8] = 150
-	ftm_origin_y[8] = 150
+	ftm_origin_x[8] = 1
+	ftm_origin_y[8] = 1
 	ftm_unit[9] = "unit-ballista"
 	ftm_origin[9] = "unit-human-barracks"
 	ftm_cost[9] = 125
-	ftm_origin_x[9] = 150
-	ftm_origin_y[9] = 150
+	ftm_origin_x[9] = 1
+	ftm_origin_y[9] = 1
 	ftm_unit[10] = "unit-attack-peasant"
 	ftm_origin[10] = "unit-town-hall"
 	ftm_cost[10] = 15
-	ftm_origin_x[10] = 255
+	ftm_origin_x[10] = 1
 	ftm_origin_y[10] = 1
 	ftm_unit[11] = "unit-dwarves"
 	ftm_origin[11] = "unit-inventor"
-	ftm_cost[11] = 150
-	ftm_origin_x[11] = 150
-	ftm_origin_y[11] = 150
+	ftm_cost[11] = 1
+	ftm_origin_x[11] = 1
+	ftm_origin_y[11] = 1
 	ftm_unit[12] = "unit-yeoman"
 	ftm_origin[12] = "unit-town-hall"
 	ftm_cost[12] = 50
-	ftm_origin_x[12] = 255
+	ftm_origin_x[12] = 1
 	ftm_origin_y[12] = 1
 	ftm_unit[13] = "unit-gryphon-rider"
 	ftm_origin[13] = "unit-gryphon-aviary"
-	ftm_cost[13] = 150
-	ftm_origin_x[13] = 150
-	ftm_origin_y[13] = 150
+	ftm_cost[13] = 1
+	ftm_origin_x[13] = 1
+	ftm_origin_y[13] = 1
 	ftm_unit[14] = "unit-mage"
 	ftm_origin[14] = "unit-mage-tower"
 	ftm_cost[14] = 100
-	ftm_origin_x[14] = 150
-	ftm_origin_y[14] = 150
+	ftm_origin_x[14] = 1
+	ftm_origin_y[14] = 1
 	ftm_unit[15] = "unit-peasant"
 	ftm_origin[15] = "unit-town-hall"
 	ftm_cost[15] = 25
-	ftm_origin_x[15] = 255
+	ftm_origin_x[15] = 1
 	ftm_origin_y[15] = 1
 	
-	
-	
-
-	CreateUnit("unit-human-barracks", 1, {149, 149})
+	ftm_unit[51] = "unit-grunt"
+	ftm_origin[51] = "unit-orc-barracks"
+	ftm_cost[51] = 25
+	ftm_origin_x[51] = 1
+	ftm_origin_y[51] = 1
+	ftm_unit[52] = "unit-quick-blade"
+	ftm_origin[52] = "unit-orc-barracks"
+	ftm_cost[52] = 200
+	ftm_origin_x[52] = 1
+	ftm_origin_y[52] = 1
+	ftm_unit[53] = "unit-axethrower"
+	ftm_origin[53] = "unit-orc-barracks"
+	ftm_cost[53] = 25
+	ftm_origin_x[53] = 1
+	ftm_origin_y[53] = 1
+	ftm_unit[54] = "unit-berserker"
+	ftm_origin[54] = "unit-orc-barracks"
+	ftm_cost[54] = 50
+	ftm_origin_x[54] = 1
+	ftm_origin_y[54] = 1
+	ftm_unit[55] = "unit-sharp-axe"
+	ftm_origin[55] = "unit-orc-barracks"
+	ftm_cost[55] = 200
+	ftm_origin_x[55] = 1
+	ftm_origin_y[55] = 1
+	ftm_unit[56] = "unit-ogre"
+	ftm_origin[56] = "unit-orc-barracks"
+	ftm_cost[56] = 75
+	ftm_origin_x[56] = 1
+	ftm_origin_y[56] = 1
+	ftm_unit[57] = "unit-ogre-mage"
+	ftm_origin[57] = "unit-orc-barracks"
+	ftm_cost[57] = 125
+	ftm_origin_x[57] = 1
+	ftm_origin_y[57] = 1
+	ftm_unit[58] = "unit-knight-rider"
+	ftm_origin[58] = "unit-orc-barracks"
+	ftm_cost[58] = 200
+	ftm_origin_x[58] = 1
+	ftm_origin_y[58] = 1
+	ftm_unit[59] = "unit-catapult"
+	ftm_origin[59] = "unit-orc-barracks"
+	ftm_cost[59] = 125
+	ftm_origin_x[59] = 1
+	ftm_origin_y[59] = 1
+	ftm_unit[60] = "unit-skeleton"
+	ftm_origin[60] = "unit-great-hall"
+	ftm_cost[60] = 15
+	ftm_origin_x[60] = 1
+	ftm_origin_y[60] = 1
+	ftm_unit[61] = "unit-goblin-sappers"
+	ftm_origin[61] = "unit-alchemist"
+	ftm_cost[61] = 1
+	ftm_origin_x[61] = 1
+	ftm_origin_y[61] = 1
+	ftm_unit[62] = "unit-nomad"
+	ftm_origin[62] = "unit-great-hall"
+	ftm_cost[62] = 50
+	ftm_origin_x[62] = 1
+	ftm_origin_y[62] = 1
+	ftm_unit[63] = "unit-dragon"
+	ftm_origin[63] = "unit-dragon-roost"
+	ftm_cost[63] = 1
+	ftm_origin_x[63] = 1
+	ftm_origin_y[63] = 1
+	ftm_unit[64] = "unit-death-knight"
+	ftm_origin[64] = "unit-temple-of-the-damned"
+	ftm_cost[64] = 100
+	ftm_origin_x[64] = 1
+	ftm_origin_y[64] = 1
+	ftm_unit[65] = "unit-peon"
+	ftm_origin[65] = "unit-great-hall"
+	ftm_cost[65] = 25
+	ftm_origin_x[65] = 255
+	ftm_origin_y[65] = 1
 	
 	ftm_team[0] = 1
 	ftm_team[1] = 2
@@ -133,26 +203,38 @@ function AiRedRibbon_Setup_2014()
 	ftm_team_x2[2] = 256
 	ftm_team_y2[2] = 256
 	ftm_team[3] = 1
+	ftm_team_x1[3] = 0
+	ftm_team_y1[3] = 0
+	ftm_team_x2[3] = 256
+	ftm_team_y2[3] = 256
 	
 	ftm_choice[0] = 3
 	ftm_choice[1] = 2
 	
 	timer[0] = 1
 	timer[1] = 1
+	
+	ftm_index_start[0] = 51
+	ftm_index_end[0] = 65
+	ftm_index_start[1] = 1
+	ftm_index_end[1] = 15
 end
 
 function AiRedRibbon_2014()
-	if (ftm_unit[1] == nil) then
-		AiRedRibbon_Setup_2014()
-	end
+	--if (ftm_unit ~= {}) then
+		--AiRedRibbon_Setup_2014()
+	--end
 	AiNephrite_Expand_2013()
-	if ((timer[AiPlayer()] == 50) or (timer[AiPlayer()] == 100)) then
+	if ((timer[AiPlayer()] == 15) or (timer[AiPlayer()] == 50)) then
+		AddMessage("Time to spawn in.")
 		if (ftm_team[AiPlayer()] == ftm_team[ftm_choice[AiPlayer()]]) then
-			for i=1,15 do
-				if ((GetNumUnitsAt(AiPlayer(), ftm_origin[i], {(ftm_origin_x[i] - 3), (ftm_origin_y[i] - 3)}, {(ftm_origin_x[i] + 3), (ftm_origin_y[i] + 3)}) > 0) and (GetNumUnitsAt(ftm_choice[AiPlayer()], ftm_unit[i], {ftm_team_x1[ftm_choice[AiPlayer()]], ftm_team_y1[ftm_choice[AiPlayer()]]}, {ftm_team_x2[ftm_choice[AiPlayer()]], ftm_team_y2[ftm_choice[AiPlayer()]]}) > 0)) then
-					AddMessage(ftm_unit[i])
-					for j=1,GetNumUnitsAt(ftm_choice[AiPlayer()], ftm_unit[i], {ftm_team_x1[ftm_choice[AiPlayer()]], ftm_team_y1[ftm_choice[AiPlayer()]]}, {ftm_team_x2[ftm_choice[AiPlayer()]], ftm_team_y2[ftm_choice[AiPlayer()]]}) do
-						CreateUnit(ftm_unit[i], AiPlayer(), {ftm_origin_x[i], ftm_origin_y[i]})
+			for i=ftm_index_start[AiPlayer()],ftm_index_end[AiPlayer()] do
+				if (GetNumUnitsAt(AiPlayer(), ftm_origin[i], {(ftm_origin_x[i] - 3), (ftm_origin_y[i] - 3)}, {(ftm_origin_x[i] + 3), (ftm_origin_y[i] + 3)}) > 0) then
+					if (GetNumUnitsAt(ftm_choice[AiPlayer()], ftm_unit[i], {ftm_team_x1[ftm_choice[AiPlayer()]], ftm_team_y1[ftm_choice[AiPlayer()]]}, {ftm_team_x2[ftm_choice[AiPlayer()]], ftm_team_y2[ftm_choice[AiPlayer()]]}) > 0) then
+						AddMessage(ftm_unit[i])
+						for j=1,GetNumUnitsAt(ftm_choice[AiPlayer()], ftm_unit[i], {ftm_team_x1[ftm_choice[AiPlayer()]], ftm_team_y1[ftm_choice[AiPlayer()]]}, {ftm_team_x2[ftm_choice[AiPlayer()]], ftm_team_y2[ftm_choice[AiPlayer()]]}) do
+							CreateUnit(ftm_unit[i], AiPlayer(), {ftm_origin_x[i], ftm_origin_y[i]})
+						end
 					end
 				end
 			end
@@ -160,10 +242,22 @@ function AiRedRibbon_2014()
 		end
 	elseif ((timer[AiPlayer()] == 25) or (timer[AiPlayer()] == 75)) then
 		AiNephrite_Attack_2013()
-	elseif ((timer[AiPlayer()] == 35) or (timer[AiPlayer()] == 85)) then
-		AiNephrite_Flush_2013()
 	elseif ((GetPlayerData(AiPlayer(), "UnitTypesCount", AiEliteShooter()) > 0) and (GetPlayerData(AiPlayer(), "UnitTypesCount", AiCavalryMage()) > 0)) then
 		AiNephrite_Research_2013()
+	elseif (timer[AiPlayer()] == 1) then	
+		for i=0,3 do
+			if (ftm_team[AiPlayer()] == ftm_team[i]) then
+				SetSharedVision(AiPlayer(), true, ftm_team[i])
+				SetSharedVision(ftm_team[i], true, AiPlayer())
+				SetDiplomacy(AiPlayer(), "allied", ftm_team[i])
+				SetDiplomacy(ftm_team[i], "allied", AiPlayer())
+			else
+				SetSharedVision(AiPlayer(), false, ftm_team[i])
+				SetSharedVision(ftm_team[i], false, AiPlayer())
+				SetDiplomacy(AiPlayer(), "enemy", ftm_team[i])
+				SetDiplomacy(ftm_team[i], "enemy", AiPlayer())
+			end
+		end
 	end
 	timer[AiPlayer()] = timer[AiPlayer()] + 1
 	if (timer[AiPlayer()] == 101) then
