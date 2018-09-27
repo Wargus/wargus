cd6248718068314541b2aa79be98ef23f3824167
Tim Felgentreff <timfelgentreff@gmail.com>
2018-09-27 18:49:32 +0000

    make soundfont a required dependency and add an (optional) copy of it to the contrib data. Fixes #256

--- wargus.cpp
+++ wargus.cpp
@@ -34,11 +34,22 @@
 #define EXTRACTOR_TOOL "wartool"
 #define EXTRACTOR_ARGS "-v -r"
 #define CHECK_EXTRACTED_VERSION 1
-#define CONTRIB_DIRECTORIES { "campaigns", "campaigns", \
-			      "contrib", "graphics/ui", \
-			      "maps", "maps",		\
-			      "shaders", "shaders",	\
-			      "scripts", "scripts", NULL }
+
+#define __wargus_contrib__ "campaigns", "campaigns", \
+                           "contrib", "graphics/ui", \
+                           "maps", "maps", \
+                           "shaders", "shaders", \
+                           "scripts", "scripts", \
+                           ":optional:", \
+                           "music/TimGM6mb.sf2", "music/TimGM6mb.sf2"
+
+#ifdef WIN32
+#define CONTRIB_DIRECTORIES { __wargus_contrib__, NULL }
+#else
+// for convenience during development, we also try to copy the system
+// soundfont to the data directory on linux
+#define CONTRIB_DIRECTORIES { __wargus_contrib__, "/usr/share/sounds/sf2/TimGM6mb.sf2", "music/TimGM6mb.sf2", NULL }
+#endif
 #define GAME_SHOULD_EXTRACT_AGAIN (tinyfd_messageBox("Extract more?", \
                                                      "Extract from an additional expansion CD?", \
                                                      "yesno",           \
