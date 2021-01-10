12549c3974e3a4cf60029733accdb6963e52e96d
Tim Felgentreff <timfelgentreff@gmail.com>
2021-01-10 11:07:08 +0100

    instead of probabilities and dependency stuff, just make the past N actions part of the state

--- scripts/ai/delayed_reward_ai.lua
+++ scripts/ai/delayed_reward_ai.lua
@@ -135,92 +135,90 @@ doSleep = function()
 end
 
 requestBuild = function(unittype, etaScale, onlyOne, isUpgrade, assignToForce)
+   AiSleep(5)
    local ply = AiPlayer()
    local idx = stratagus.gameData.AIState.index[ply + 1]
    dbg(ply, unittype)
 
-   local totalBuildings = GetPlayerData(ply, "TotalBuildings")
-   if totalBuildings == 0 then
-      -- only allow city center as the first building
-      if unittype ~= AiCityCenter() then
-         return false
-      end
-   end
+   -- local totalBuildings = GetPlayerData(ply, "TotalBuildings")
+   -- if totalBuildings == 0 then
+   --    -- only allow city center as the first building
+   --    if unittype ~= AiCityCenter() then
+   --       return false
+   --    end
+   -- end
 
-   if idx.buildETA[unittype] ~= nil and idx.buildETA[unittype] ~= 0 then
-      -- we only allow the AI to build one unit per type at a time
-      return false
-   end
+   -- if idx.buildETA[unittype] ~= nil and idx.buildETA[unittype] ~= 0 then
+   --    -- we only allow the AI to build one unit per type at a time
+   --    return false
+   -- end
 
    local currentUnitCount = GetPlayerData(ply, "UnitTypesCount", unittype)
-   if onlyOne and (currentUnitCount > 0) then
-      return false
-   end
+   -- if onlyOne and (currentUnitCount > 0) then
+   --    return false
+   -- end
 
-   local goldCost = GetUnitTypeData(unittype, "Costs", "gold")
-   local goldAvail = GetPlayerData(ply, "Resources", "gold")
-   if goldAvail < goldCost then
-      return false
-   end
+   -- local goldCost = GetUnitTypeData(unittype, "Costs", "gold")
+   -- local goldAvail = GetPlayerData(ply, "Resources", "gold")
+   -- if goldAvail < goldCost then
+   --    return false
+   -- end
 
-   local woodCost = GetUnitTypeData(unittype, "Costs", "wood")
-   local woodAvail = GetPlayerData(ply, "Resources", "wood")
-   if woodAvail < woodCost then
-      return false
-   end
+   -- local woodCost = GetUnitTypeData(unittype, "Costs", "wood")
+   -- local woodAvail = GetPlayerData(ply, "Resources", "wood")
+   -- if woodAvail < woodCost then
+   --    return false
+   -- end
 
-   local oilCost = GetUnitTypeData(unittype, "Costs", "oil")
-   local oilAvail = GetPlayerData(ply, "Resources", "oil")
-   if oilAvail < oilCost then
-      return false
-   end
+   -- local oilCost = GetUnitTypeData(unittype, "Costs", "oil")
+   -- local oilAvail = GetPlayerData(ply, "Resources", "oil")
+   -- if oilAvail < oilCost then
+   --    return false
+   -- end
 
    -- the micro-AI in C handles supply and demand automatically, but we want to
    -- give the external agent more control
-   local demand = GetUnitTypeData(unittype, "Demand")
-   local supply = GetPlayerData(ply, "Supply")
-   if supply < demand then
-      return false
-   end
+   -- local demand = GetUnitTypeData(unittype, "Demand")
+   -- local supply = GetPlayerData(ply, "Supply")
+   -- if supply < demand then
+   --    return false
+   -- end
 
-   if not CheckDependency(ply, unittype) then
-      return false
-   end
+   -- if not CheckDependency(ply, unittype) then
+   --    return false
+   -- end
 
    -- we do not allow queueing for the AI. A production unit must be available
    -- immediately, otherwise our probability for spawning is off
-   local producers = unitToBuilderMap[unittype]
-   if producers == nil then
-      return false
-   end
-   local availableSlots = 0
-   for _,producer in ipairs(producers) do
-      availableSlots = availableSlots + GetPlayerData(ply, "UnitTypesCount", producer)
-      for i,v in ipairs(builderToUnitsMap[producer]) do
-         if idx.buildETA[v] and idx.buildETA[v] > 0 then
-            availableSlots = availableSlots - 1
-         end
-      end
-   end
-   if availableSlots <= 0 then
-      return false
-   end
-
-   -- always request a bit of sleep, so that the build can get queued
-   AiSleep(10)
+   -- local producers = unitToBuilderMap[unittype]
+   -- if producers == nil then
+   --    return false
+   -- end
+   -- local availableSlots = 0
+   -- for _,producer in ipairs(producers) do
+   --    availableSlots = availableSlots + GetPlayerData(ply, "UnitTypesCount", producer)
+   --    for i,v in ipairs(builderToUnitsMap[producer]) do
+   --       if idx.buildETA[v] and idx.buildETA[v] > 0 then
+   --          availableSlots = availableSlots - 1
+   --       end
+   --    end
+   -- end
+   -- if availableSlots <= 0 then
+   --    return false
+   -- end
 
    -- for buildings we will get a scale, to account for workers to walk to the
    -- build location. This means the probability for buildings is less reliable,
    -- but that's ok
-   local timeCost = GetUnitTypeData(unittype, "Costs", "time")
-   if etaScale == nil then
-      etaScale = 1
-   end
-   idx.buildETA[unittype] = GameCycle + (etaScale * 6 * timeCost)
+   -- local timeCost = GetUnitTypeData(unittype, "Costs", "time")
+   -- if etaScale == nil then
+   --    etaScale = 1
+   -- end
+   -- idx.buildETA[unittype] = GameCycle + (etaScale * 6 * timeCost)
    if isUpgrade then
       return AiUpgradeTo(unittype)
    elseif assignToForce then
-      idx.ForceSize = idx.ForceSize + goldCost + woodCost + oilCost
+      -- idx.ForceSize = idx.ForceSize + goldCost + woodCost + oilCost
       return AiForce(assignToForce, {unittype, currentUnitCount + 1})
    else
       return AiSet(unittype, currentUnitCount + 1)
@@ -296,8 +294,6 @@ local stateVariables = {
    -- return three states for game cycle: late game (2), mid game (1), early game (0)
    function(ply) if GameCycle > 60000 then return 2 elseif GameCycle > 20000 then return 1 else return 0 end end,
 
-   function(ply) return getRazingsProbability(ply) end,
-
    function(ply) return getResourceValue(ply, "gold") end,
    function(ply) return getResourceValue(ply, "wood") end,
    function(ply) return getResourceValue(ply, "oil") end,
@@ -309,18 +305,9 @@ local stateVariables = {
    function(ply) return boxUnitCountValues(GetPlayerData(ply, "UnitTypesCount", AiMage())) end,
    function(ply) return boxUnitCountValues(GetPlayerData(ply, "UnitTypesCount", AiCatapult())) end,
    function(ply) return boxUnitCountValues(GetPlayerData(ply, "UnitTypesCount", AiFlyer())) end,
-   function(ply) return getSpawnProbability(ply, AiWorker()) end,
-   function(ply) return getSpawnProbability(ply, AiSoldier()) end,
-   function(ply) return getSpawnProbability(ply, AiShooter()) end,
-   function(ply) return getSpawnProbability(ply, AiCavalry()) end,
-   function(ply) return getSpawnProbability(ply, AiMage()) end,
-   function(ply) return getSpawnProbability(ply, AiCatapult()) end,
-   function(ply) return getSpawnProbability(ply, AiFlyer()) end,
 
    function(ply) return boxUnitCountValues(GetPlayerData(ply, "UnitTypesCount", AiGuardTower())) end,
    function(ply) return boxUnitCountValues(GetPlayerData(ply, "UnitTypesCount", AiCannonTower())) end,
-   function(ply) return getSpawnProbability(ply, AiGuardTower()) end,
-   function(ply) return getSpawnProbability(ply, AiCannonTower()) end,
 
    function(ply) return GetPlayerData(ply, "UnitTypesCount", AiFarm()) end,
    function(ply) return GetPlayerData(ply, "UnitTypesCount", AiCityCenter()) end,
@@ -329,13 +316,6 @@ local stateVariables = {
    function(ply) return GetPlayerData(ply, "UnitTypesCount", AiMageTower()) end,
    function(ply) return GetPlayerData(ply, "UnitTypesCount", AiAirport()) end,
    function(ply) return GetPlayerData(ply, "UnitTypesCount", AiBarracks()) end,
-   function(ply) return getSpawnProbability(ply, AiFarm()) end,
-   function(ply) return getSpawnProbability(ply, AiCityCenter()) end,
-   function(ply) return getSpawnProbability(ply, AiBetterCityCenter()) end,
-   function(ply) return getSpawnProbability(ply, AiBestCityCenter()) end,
-   function(ply) return getSpawnProbability(ply, AiMageTower()) end,
-   function(ply) return getSpawnProbability(ply, AiAirport()) end,
-   function(ply) return getSpawnProbability(ply, AiBarracks()) end,
 
    -- these buildings are only needed once, so we only return 0 or 1
    function(ply) return math.min(GetPlayerData(ply, "UnitTypesCount", AiLumberMill()), 1) end,
@@ -343,11 +323,6 @@ local stateVariables = {
    function(ply) return math.min(GetPlayerData(ply, "UnitTypesCount", AiScientific()), 1) end,
    function(ply) return math.min(GetPlayerData(ply, "UnitTypesCount", AiStables()), 1) end,
    function(ply) return math.min(GetPlayerData(ply, "UnitTypesCount", AiTemple()), 1) end,
-   function(ply) return getSpawnProbability(ply, AiLumberMill()) end,
-   function(ply) return getSpawnProbability(ply, AiBlacksmith()) end,
-   function(ply) return getSpawnProbability(ply, AiScientific()) end,
-   function(ply) return getSpawnProbability(ply, AiStables()) end,
-   function(ply) return getSpawnProbability(ply, AiTemple()) end,
 
    -- function(ply) return GetPlayerData(ply, "UnitTypesCount", AiHarbor()) end,
    -- function(ply) return GetPlayerData(ply, "UnitTypesCount", AiRefinery()) end,
@@ -381,8 +356,82 @@ local stateVariables = {
    -- function(ply) return countEnemyUnits(ply, AiBattleship) end,
 }
 
+-- last actions are part of the state. because actions have delayed effects,
+-- making them part of the state should allow the agent to learn that
+-- eventually actions will have an outcome
+
+Queue = {}
+Queue.__index = Queue
+function Queue:new(cap)
+   local qu = {}
+   setmetatable(qu, Queue)
+   qu.head = 1
+   qu.cap = cap
+   qu.size = 0
+   qu.data = {}
+   return qu
+end
+
+function Queue:push(el)
+   local head = self.head
+   local size = self.size
+   local cap = self.cap
+   local newIndex = (self.head - 1 + size) % cap + 1
+   if size == cap then
+      self:pop() -- remove front item if we're now larger than cap
+   end
+   self.size = self.size + 1
+   self.data[newIndex] = el
+end
+
+function Queue:peek(idx)
+   if idx == nil then
+      idx = self.head
+   else
+      idx = idx - 1 + self.head
+   end
+   return self.data[((idx - 1) % self.cap) + 1]
+end
+
+function Queue:pop()
+   local head = self.head
+   local size = self.size
+   local cap = self.cap
+   local el = self.data[head]
+   self.data[head] = nil
+
+   self.size = size - 1
+   if size == 0 then
+      head = 1 -- reset to front when we're emtpy
+   elseif head == self.cap then
+      self.head = 1
+   else
+      self.head = head + 1
+   end
+   return el
+end
+
+local makeAiHistoryFunc = function(i)
+   return function(ply)
+      local idx = stratagus.gameData.AIState.index[ply + 1]
+      local act = idx.actionHistory:peek(i)
+      if act == nil then
+         return 0
+      else
+         return act
+      end
+   end
+end
+local AiActionHistorySize = 40
+for i=1,AiActionHistorySize,1 do
+   stateVariables[#stateVariables + 1] = makeAiHistoryFunc(i)
+end
+
 -- each "action" the processor asks for is actually an AI loop to do for a while
 local actions = {
+   -- sleep should be action 0, for nothing
+   { function() return doSleep() end },
+
    { function() return requestBuilding(AiFarm()) end },
    { function() return requestBuilding(AiCityCenter(), true) end },
    { function() return requestBuilding(AiBarracks()) end },
@@ -420,7 +469,6 @@ local actions = {
    -- { function() return requestResearch(AiUpgradeEliteShooter2()) end },
    -- { function() return requestResearch(AiUpgradeEliteShooter3()) end },
 
-   { function() return doSleep() end },
    { function() return doAttack(1) end },
 }
 
@@ -435,7 +483,7 @@ local prepareStepArgs = function(playerIndex)
    return 0, env
 end
 
-function MakeAi(port)
+local MakeAi = function(port)
    return function()
       local playerIndex = AiPlayer() + 1
 
@@ -454,7 +502,8 @@ function MakeAi(port)
             ForceSize = 0, -- current number of requested units in force
             attack = 0, -- current value of underway attack forces
             punishment = 0,
-            buildETA = {} -- current ETAs for build/train requests
+            buildETA = {}, -- current ETAs for build/train requests
+            actionHistory = Queue:new(AiActionHistorySize),
          }
          stratagus.gameData.AIState.index[playerIndex] = idx
 
@@ -466,9 +515,9 @@ function MakeAi(port)
                   print("WIN")
                   next_reward = next_reward + 1000
                elseif GetPlayerData(playerIndex - 1, "TotalNumUnits") == 0 then -- loss
-                  -- we punish the AI less if it put up a good fight -- -100..-1
-                  improvement = math.min(GetPlayerData(playerIndex - 1, "TotalKills") + GetPlayerData(playerIndex - 1, "TotalRazings") * 2, 99)
-                  next_reward = next_reward - 100 + improvement
+                  -- we punish the AI less if it put up a good fight -- -1000..-500
+                  improvement = math.min(GetPlayerData(playerIndex - 1, "TotalKills") + GetPlayerData(playerIndex - 1, "TotalRazings") * 2, 500)
+                  next_reward = next_reward - 1000 + improvement
                else -- draw, reward a little bit for kills and razings -- 0..50
                   next_reward = next_reward + math.min(GetPlayerData(playerIndex - 1, "TotalKills") + GetPlayerData(playerIndex - 1, "TotalRazings") * 2, 50)
                end
@@ -490,7 +539,8 @@ function MakeAi(port)
          -- ask agent what to do next
          next_reward, env = prepareStepArgs(playerIndex)
          dbg("Reward", next_reward)
-         idx.ActionIndex = AiProcessorStep(idx.Socket, next_reward, env) + 1
+         idx.ActionIndex = AiProcessorStep(idx.Socket, next_reward, env)
+         idx.actionHistory:push(idx.ActionIndex)
          idx.LoopIndex = 1
       else
          -- work on action that the agent requested
