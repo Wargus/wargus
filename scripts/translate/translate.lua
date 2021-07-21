a3a7bff3b04baa66c6b431271c111376d550f85f
Tim Felgentreff <timfelgentreff@gmail.com>
2021-07-21 15:51:14 +0200

    add german translation

--- scripts/translate/translate.lua
+++ scripts/translate/translate.lua
@@ -1,5 +1,6 @@
 LanguageTable = {
 	"English", "", "",
+	"German", "scripts/translate/stratagus-de.po", "scripts/translate/de_DE.po",
 	"Italian", "scripts/translate/stratagus-it.po", "scripts/translate/it_IT.po",
 	"Spanish", "scripts/translate/stratagus-es.po", "scripts/translate/es_ES.po",
 	"Russian", "scripts/translate/stratagus-ru.po", "scripts/translate/ru_RU.po",
