0cf23c45c9ecc11448f663584a0d4e1de7848ea6
psa-jforestier <jerome.forestier@mpsa.com>
2021-08-04 16:04:04 +0200

    Add fr translation available in menu

--- scripts/translate/translate.lua
+++ scripts/translate/translate.lua
@@ -3,4 +3,5 @@ LanguageTable = {
 	"Italian", "scripts/translate/stratagus-it.po", "scripts/translate/it_IT.po",
 	"Spanish", "scripts/translate/stratagus-es.po", "scripts/translate/es_ES.po",
 	"Russian", "scripts/translate/stratagus-ru.po", "scripts/translate/ru_RU.po",
+	"French", "scripts/translate/stratagus-fr.po", "scripts/translate/fr_FR.po",
 }
