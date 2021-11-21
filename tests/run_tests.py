874426de74ac751911e553a6746260ea02bbbb82
Alex Benedict <AlexGrantBenedict@gmail.com>
2021-11-21 12:58:12 -0700

    Initial test running

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -2,6 +2,7 @@ import argparse
 import pathlib
 import sys
 import hashlib
+import subprocess
 import generate_test_case
 
 parser = argparse.ArgumentParser(
@@ -17,7 +18,11 @@ parser.add_argument(
     type=pathlib.Path,
     help="The path to the directory where wartool should output the new files.",
 )
-
+parser.add_argument(
+    "wartool_exe",
+    type=pathlib.Path,
+    help="The path to the version of wartool to test.",
+)
 
 def find_test_case(dir_hash):
     cur_path = pathlib.Path(".")
@@ -30,7 +35,8 @@ def find_test_case(dir_hash):
     return None
             
 
-def run_test(original_data, new_output):
+def run_test(original_data, new_output, executable):
+    errors = False
     original_directory_hash = generate_test_case.hash_dir(original_data)
     test_case = find_test_case(original_directory_hash)
     if test_case == None:
@@ -50,13 +56,35 @@ def run_test(original_data, new_output):
             file_hash = sections[-1]
             file_name = ' '.join(sections[:-1])
             file_hash_mapping[file_name] = file_hash
-    print(file_hash_mapping)
+    subprocess.run([executable, original_data, new_output])
+    file_list = generate_test_case.get_file_list_from_root_dir(new_output)
+    out_file.write("Found " + str(len(output_file_list)) + " files.\n")
+    if len(output_file_list) != file_count:
+        print("Wrong number of files!\nExpected " + str(file_count)+" but found " + len(output_file_list))
+        errors = True
+    new_file_hash_mapping = dict()
+    for item in output_file_list:
+        h = hashlib.blake2b(digest_size=20)
+        with open(item, "rb") as temp_file:
+            h.update(temp_file.read())
+        new_file_hash_mapping[item.relative_to(new_output).name] = h.hexdigest()
+    for key, value in new_file_hash_mapping.items():
+        try: 
+            if file_hash_mapping[key] != value:
+                print("Disagreement on file: " + key)
+                errors = True
+        except:
+            print("Error checking file: " + key)
+            errors=True
+    if errors:
+        sys.exit(3)
+        
 
 
 def main():
     args = parser.parse_args()
     run_test(
-        args.original_data_dir, args.wartool_output_dir
+        args.original_data_dir, args.wartool_output_dir,args.wartool_exe
     )
     return 0
 
