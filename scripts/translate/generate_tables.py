54d50cb254a0ea368e326e519bda6982fb11ff92
Tim Felgentreff <timfelgentreff@gmail.com>
2022-01-27 13:00:11 +0100

    update mapping table gen

--- scripts/translate/generate_tables.py
+++ scripts/translate/generate_tables.py
@@ -2,76 +2,60 @@
 
 from unidecode import unidecode
 
-print("static const unsigned char utf8_europe_to_cp437[] = {")
-for i in range(0xa0, 0xff + 1):
-    if i % 16 == 0:
-        print("    ", end="")
-    print("0x%02x" % chr(i).encode("cp437", errors="replace").replace(b'?', b'\x00')[0], ", ", sep="", end="")
-    if i % 16 == 15:
-        print(f"// {hex(i - 15)}")
-print("};\n")
 
-print("static const unsigned char utf8_europe_to_cp1252[] = {")
-for i in range(0xa0, 0xff + 1):
-    if i % 16 == 0:
-        print("    ", end="")
-    print("0x%02x" % chr(i).encode("cp1252", errors="replace").replace(b'?', b'\x00')[0], ", ", sep="", end="")
-    if i % 16 == 15:
-        print(f"// {hex(i - 15)}")
-print("};\n")
+ESCAPE = bytes([ord('\\')])
 
-print("static const unsigned char utf8_europe_to_ascii[] = {")
-for i in range(0xa0, 0xff + 1):
-    if i % 16 == 0:
-        print("    ", end="")
-    replacement = unidecode(chr(i)) or "\x00"
-    print("0x%02x" % replacement.encode('ascii')[0], ", ", sep="", end="")
-    if i % 16 == 15:
-        print(f"// {hex(i - 15)}")
-print("};\n")
 
-print("static const unsigned char utf8_cyrillic_to_ascii[] = {")
-for i in range(0x400, 0x45f + 1):
-    if i % 16 == 0:
-        print("    ", end="")
-    replacement = unidecode(chr(i)) or "\x00"
-    print("0x%02x" % replacement.encode('ascii')[0], ", ", sep="", end="")
-    if i % 16 == 15:
-        print(f"// {hex(i - 15)}")
-print("};\n")
+def print_reverse_mapping_for_codepage(cp, start, end):
+    print(f"static const unsigned char utf8_to_{cp}[] = {{")
+    for i in range(start, end + 1):
+        if i % 16 == 0:
+            print("    ", end="")
+        replacement = chr(i).encode(cp, errors="replace").replace(b'?', b'\x00')
+        assert len(replacement) == 1
+        print("0x%02x" % replacement[0], end="")
+        if i != end:
+            print(", ", end="")
+        else:
+            print("  ", end="")
+        if i % 16 == 15:
+            print(f"// {hex(i - 15)}")
+    print("};\n")
 
-print("static const unsigned char utf8_cyrillic_to_cp866[] = {")
-for i in range(0x400, 0x45f + 1):
-    if i % 16 == 0:
-        print("    ", end="")
-    print("0x%02x" % chr(i).encode("cp866", errors="replace").replace(b'?', b'\x00')[0], ", ", sep="", end="")
-    if i % 16 == 15:
-        print(f"// {hex(i - 15)}")
-print("};\n")
 
-print("static const char *cp437_to_utf8[] = {")
-for i in range(0, 256):
-    if i % 8 == 0:
-        print("    ", end="")
-    print('%14s, ' % f'"{repr(bytes([i]).decode("cp437").encode("utf-8"))[2:-1]}"', end="")
-    if i % 8 == 7:
-        print(f"// {hex(i - 7)}")
-print("};\n")
+print_reverse_mapping_for_codepage("cp437", 0xa0, 0xff)
+print_reverse_mapping_for_codepage("cp1252", 0xa0, 0xff)
+print_reverse_mapping_for_codepage("cp866", 0x400, 0x45f)
 
-print("static const char *cp1252_to_utf8[] = {")
-for i in range(0, 256):
-    if i % 8 == 0:
-        print("    ", end="")
-    print('%14s, ' % f'"{repr(bytes([i]).decode("cp1252", errors="replace").encode("utf-8"))[2:-1]}"', end="")
-    if i % 8 == 7:
-        print(f"// {hex(i - 7)}")
-print("};\n")
 
-print("static const char *cp866_to_utf8[] = {")
-for i in range(0, 256):
-    if i % 8 == 0:
-        print("    ", end="")
-    print('%14s, ' % f'"{repr(bytes([i]).decode("cp866").encode("utf-8"))[2:-1]}"', end="")
-    if i % 8 == 7:
-        print(f"// {hex(i - 7)}")
-print("};\n")
+def print_ascii_mapping_for_range(start, end):
+    print(f"static const char * utf8_from_{hex(start)}_to_ascii[] = {{")
+    for i in range(start, end + 1):
+        if i % 16 == 0:
+            print("    ", end="")
+        replacement = repr((unidecode(chr(i)) or "?").encode('ascii').replace(b'"', ESCAPE + b'"'))[2:-1].replace(r'\\', '\\')
+        print("%8s, " % f'"{replacement}"', end="")
+        if i % 16 == 15:
+            print(f"// {hex(i - 15)}")
+    print("};\n")
+
+
+print_ascii_mapping_for_range(0xa0, 0xff)
+print_ascii_mapping_for_range(0x400, 0x45f)
+
+
+def print_mapping_to_utf(cp):
+    print(f"static const char *{cp}_to_utf8[] = {{")
+    for i in range(0, 256):
+        if i % 8 == 0:
+            print("    ", end="")
+        replacement = bytes([i]).decode(cp, errors="replace").encode("utf-8").replace(b'"', ESCAPE + b'"')
+        print('%14s, ' % f'"{repr(replacement)[2:-1]}"', end="")
+        if i % 8 == 7:
+            print(f"// {hex(i - 7)}")
+    print("};\n")
+
+
+print_mapping_to_utf("cp437")
+print_mapping_to_utf("cp1252")
+print_mapping_to_utf("cp866")
