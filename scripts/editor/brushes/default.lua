355e6d66fce7c7730270df08dc0f3269ab5382d1
alyokhin <ipochto@gmail.com>
2025-03-08 00:34:59 +0300

    Added base brushes

---
+++ scripts/editor/brushes/default.lua
@@ -0,0 +1,48 @@
+--       _________ __                 __
+--      /   _____//  |_____________ _/  |______     ____  __ __  ______
+--      \_____  \\   __\_  __ \__  \\   __\__  \   / ___\|  |  \/  ___/
+--      /        \|  |  |  | \// __ \|  |  / __ \_/ /_/  >  |  /\___ \
+--     /_______  /|__|  |__|  (____  /__| (____  /\___  /|____//____  >
+--             \/                  \/          \//_____/            \/
+--  ______________________                           ______________________
+--                        T H E   W A R   B E G I N S
+--         Stratagus - A free fantasy real time strategy game engine
+--
+--
+--      This program is free software; you can redistribute it and/or modify
+--      it under the terms of the GNU General Public License as published by
+--      the Free Software Foundation; either version 2 of the License, or
+--      (at your option) any later version.
+--
+--      This program is distributed in the hope that it will be useful,
+--      but WITHOUT ANY WARRANTY; without even the implied warranty of
+--      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--      GNU General Public License for more details.
+--
+--      You should have received a copy of the GNU General Public License
+--      along with this program; if not, write to the Free Software
+--      Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+--
+
+local brush = {
+  Name = "Default",
+  Type = "SingleTile", -- [Decoration]
+  Shape = "Rectangular", -- [Round]
+  Symmetric = false,
+  Allign = "UpperLeft",  -- [Center]
+  Resizable = true,
+  ResizeSteps = {1, 1},
+  MinSize = {1, 1},
+  MaxSize = {20, 20},
+  RandomizeAllowed = true,
+  FixNeighborsAllowed = true,
+  TileIconsPaletteRequired = true,
+  -- ExtendedTilesetRequired = true,
+  -- Generator = { -- Multitile brush generator's options
+    --- ["source"] = "path/to/generatror.lua"
+    --- ["optionName"] = {possibleValues, ...}
+  -- }
+}
+
+EditorAddBrush(brush)
+
